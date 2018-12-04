4187c0fa9a4b37bc61f8b14f658885fc969798ea
Artem Medvedev <art_med2@mail.ru>
2018-12-05 02:12:04 +0300

    all

--- cursach/curs/trainworld/urls.py
+++ cursach/curs/trainworld/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.info_list, name='info_list'),
     path('get_port', views.get_port, name='get_port'),
-    path('admin/', views.admin, name='admin'),
-    path('admin/auth', views.auth, name='auth'),
-    path('admin/add_port', views.add_port, name='add_port'),
+    path('auth', views.auth, name='auth'),
+    path('admin', views.admin, name='admin'),
+    path('add_port', views.add_port, name='add_port'),
+    path('add_train', views.add_train, name='add_train'),
+    path('add_manager', views.add_manager, name='add_manager')
     #path('contact/', views.contact, name='contact')
 ]
--- cursach/curs/trainworld/views.py
+++ cursach/curs/trainworld/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 import os
 import json
 from django.utils import timezone
-
+from .models import Post
 
 
 def open_file(name):
@@ -48,29 +48,54 @@ def auth(request):
 
 
 def admin(request):
-    return render(request, "trainworld/admin.html")
+    logininfo = open_file("admin")
+    for i in logininfo:
+        if request.POST.get('login') == i["login"] and request.POST.get('password') == i["password"]:
+            return render(request, "trainworld/add_train.html", {"port": request.POST.get("city")})
+
+    ports = open_file("Ports")
+    return render(request, "trainworld/main.html", {'ports': ports})
+
 
 def add_port(request):
     ports = open_file("ports")
-    newport = {"city": request.POST.get("city"),
-                       "name_port": request.POST.get("name_port"),
-                       "city": request.POST.get("city"),
-                       "status": request.POST.get("status"),
-                       "trains": [
-                           {
-                               "train": "0",
-                               "name": request.POST.get("name"),
-                               "type": request.POST.get("type"),
-                               "class": request.POST.get("class"),
-                               "arrival": request.POST.get("arrival"),
-                               "depurt": request.POST.get("depurt"),
-                               "timerun": request.POST.get("timerun"),
-                               "timeend": request.POST.get("timeend")
-                           }
-                       ]
-                       }
+    newport = {"name_port": request.POST.get("name_port"),
+               "city": request.POST.get("city"),
+               "status": request.POST.get("status"),
+               "trains": [
+               ]
+               }
     ports.append(newport)
-    print(ports)
     write_in_file("Ports", str(ports).replace("\'", "\""))
+
+    return render(request, "trainworld/main.html", {'ports': ports})
+
+
+def add_train(request):
+    ports = open_file("ports")
+    for i in ports:
+        if i["city"] == request.POST.get("city"):
+            t = {"train": str(len(i["trains"])),
+                     "name": request.POST.get("name"),
+                     "type": request.POST.get("type"),
+                     "class": request.POST.get("class"),
+                     "arrival": request.POST.get("arrival"),
+                     "depurt": request.POST.get("depurt"),
+                     "timerun": request.POST.get("timerun"),
+                     "timeend": request.POST.get("timeend")
+                     }
+            i["trains"].append(t)
+            write_in_file("ports", str(ports).replace("\'", "\""))
+
+    return render(request, "trainworld/main.html", {'ports': ports})
+
+
+def add_manager(request):
+    managers = open_file("admin")
+    ports = open_file("ports")
+    newmanager = {"login": request.POST.get("login"),
+               "password": request.POST.get("password")}
+    managers.append(newmanager)
+    write_in_file("admin", str(managers).replace("\'", "\""))
 
     return render(request, "trainworld/main.html", {'ports': ports})
